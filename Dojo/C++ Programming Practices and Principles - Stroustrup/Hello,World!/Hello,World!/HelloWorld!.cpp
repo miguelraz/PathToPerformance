14bde259f61b4e131849bea26f4b3dc72c53c111
miguelraz <miguelraz@ciencias.unam.mx>
2017-05-04 16:42:48 -0500

    update bjarne and README

--- Dojo/C++ Programming Practices and Principles - Stroustrup/Hello,World!/Hello,World!/HelloWorld!.cpp
+++ Dojo/C++ Programming Practices and Principles - Stroustrup/Hello,World!/Hello,World!/HelloWorld!.cpp
@@ -1,64 +1,44 @@
-// Ex. 17 - Quadratic root solver
+// Trythis6
 #include "std_lib_facilities.h"
 #include <vector>
 #include <iostream>
 using namespace std;
 
+class bad_ints {};
+class bad_input {};
+class bad_size {};
+class bad_overflow {};
+
 int main()
-{
-	// Inputs and setups
-	vector<int> scores;
-	vector<string> names;
-	string nam = ""; int sco = 0;
 
-	// Get names from user.
-	while (nam != "no" || sco != 0) {
-		cout << " Gimme a name and a score " << endl;
-		cin >> nam >> sco;
-		names.push_back(nam); 
-		scores.push_back(sco);
+try {
+
+
+
+	}
+} catch (exception& e) {
+		cout << "error: " << e.what() << '\n';
+		keep_window_open();
+		return 1;
+	}
+	catch (bad_overflow) {
+		cout << "error: There was an overflow issue." << endl;
+		keep_window_open();
+		return 3;
+	}
+	catch (bad_input) {
+		cout << "error: Check your input " << endl;
+		keep_window_open();
+		return 4;
+	}
+	catch (bad_ints) {
+		cout << "error: Entries were not non-negative " << endl;
+		keep_window_open();
+		return 5;
+	}
+	catch (...) {
+		cout << "Oops: unknown exception!\n";
+		keep_window_open();
+		return 2;
 	}
 
-	// Sanity test.
-	for (int i = 0; i < names.size(); ++i) cout << names[i] << " had " << scores[i] << " points " << endl;
-
-	// Check if repeated.
-	for (int i = 0; i < names.size()-1; ++i) {
-		for (int j =  i; j < names.size() - 1 ; ++i) {
-			if (names[i] == names[j+2]) {
-				cout << "names index is at " << i << " " << names[i] << " names[i]" << endl;
-				cout << "names index is at " << j << " " << names[j + 1] << " names[j+1]" << endl;
-				return 0;
-			}
-		}
-
-		cout << names[i] << " had " << scores[i] << " points " << endl;
-	}
-
-	//Exercise 20
-	//Output corresponding score to name or name not found.
-	cout << "Now gimme a name to check for " << endl;
-	string searchname; int searchscore;
-	cin >> searchname;
-	for (int x : names) {
-		if (searchname == names[x]) {
-			cout << names[x] << " had a score of " << scores[x] << endl;
-			break;
-		}
-	}
-		cout << " Name not found. " << endl;
-
-
-	//Exercise 21
-	//Output corresponding names given scores or score not found
-	int	check = 0;
-		cout << "Now gimme a score to check for " << endl;
-		for (int x : scores) {
-			if (searchschore == score[x]) {
-				cout << names[x] << " had a score of " << scores[x] << endl;
-				check += 1;
-				continue;
-			}
-		}
-		if (check == 0) cout << "Sorry, no one had that score." << endl;
-}
